900d6001d1635b2f7d6d1a6d3436f24337c2cd7a
hwj <1571317440@qq.com>
2021-03-23 11:52:57 +0800

    feat:项目创建

---
+++ mall-coupon/src/test/java/com/hwj/mall/coupon/MallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hwj.mall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
