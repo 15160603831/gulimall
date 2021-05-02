2a699ca38f1e0d2be5c9de2c16135e69b599a665
hwj <123@qq.com>
2021-05-02 23:51:04 +0800

    feat(*)_递归查询所有菜单下的子菜单

--- mall-product/src/main/java/com/hwj/mall/product/controller/PmsCategoryController.java
+++ mall-product/src/main/java/com/hwj/mall/product/controller/PmsCategoryController.java
@@ -1,21 +1,15 @@
 package com.hwj.mall.product.controller;
 
 import java.util.Arrays;
-import java.util.Map;
+import java.util.List;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.hwj.mall.product.entity.PmsCategoryEntity;
 import com.hwj.mall.product.service.PmsCategoryService;
-import com.hwj.common.utils.PageUtils;
 import com.hwj.common.utils.R;
-
 
 
 /**
@@ -32,13 +26,14 @@ public class PmsCategoryController {
     private PmsCategoryService pmsCategoryService;
 
     /**
-     * 列表
+     * 查出所有分类以及子分类，以树形结构组装
      */
-    @RequestMapping("/list")
-    public R list(@RequestParam Map<String, Object> params){
-        PageUtils page = pmsCategoryService.queryPage(params);
+    @GetMapping("/list/tree")
+    public R list() {
+        List<PmsCategoryEntity> entityList = pmsCategoryService.listWithTree();
 
-        return R.ok().put("page", page);
+
+        return R.ok().put("data", entityList);
     }
 
 
@@ -46,8 +41,8 @@ public class PmsCategoryController {
      * 信息
      */
     @RequestMapping("/info/{catId}")
-    public R info(@PathVariable("catId") Long catId){
-		PmsCategoryEntity pmsCategory = pmsCategoryService.getById(catId);
+    public R info(@PathVariable("catId") Long catId) {
+        PmsCategoryEntity pmsCategory = pmsCategoryService.getById(catId);
 
         return R.ok().put("pmsCategory", pmsCategory);
     }
@@ -56,8 +51,8 @@ public class PmsCategoryController {
      * 保存
      */
     @RequestMapping("/save")
-    public R save(@RequestBody PmsCategoryEntity pmsCategory){
-		pmsCategoryService.save(pmsCategory);
+    public R save(@RequestBody PmsCategoryEntity pmsCategory) {
+        pmsCategoryService.save(pmsCategory);
 
         return R.ok();
     }
@@ -66,8 +61,8 @@ public class PmsCategoryController {
      * 修改
      */
     @RequestMapping("/update")
-    public R update(@RequestBody PmsCategoryEntity pmsCategory){
-		pmsCategoryService.updateById(pmsCategory);
+    public R update(@RequestBody PmsCategoryEntity pmsCategory) {
+        pmsCategoryService.updateById(pmsCategory);
 
         return R.ok();
     }
@@ -76,8 +71,8 @@ public class PmsCategoryController {
      * 删除
      */
     @RequestMapping("/delete")
-    public R delete(@RequestBody Long[] catIds){
-		pmsCategoryService.removeByIds(Arrays.asList(catIds));
+    public R delete(@RequestBody Long[] catIds) {
+        pmsCategoryService.removeByIds(Arrays.asList(catIds));
 
         return R.ok();
     }
--- mall-product/src/main/java/com/hwj/mall/product/service/impl/PmsCategoryServiceImpl.java
+++ mall-product/src/main/java/com/hwj/mall/product/service/impl/PmsCategoryServiceImpl.java
@@ -1,7 +1,12 @@
 package com.hwj.mall.product.service.impl;
 
 import org.springframework.stereotype.Service;
+
+import java.util.Comparator;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -25,5 +30,45 @@ public class PmsCategoryServiceImpl extends ServiceImpl<PmsCategoryDao, PmsCateg
 
         return new PageUtils(page);
     }
+
+    @Override
+    public List<PmsCategoryEntity> listWithTree() {
+        //查出所有分类
+        List<PmsCategoryEntity> pmsCategoryEntities = baseMapper.selectList(null);
+        //组装成树形结构
+        //2.1找到所有一级分类
+        List<PmsCategoryEntity> levelMenu = pmsCategoryEntities.stream()
+                .filter(t -> t.getParentCid()==0)
+                .map((menu) -> {
+                    menu.setChildren(getChildren(menu,pmsCategoryEntities));
+                    return menu;
+                })
+                .sorted(Comparator.comparingInt(sort -> (sort.getSort() == null ? 0 : sort.getSort())))
+                .collect(Collectors.toList());
+        System.out.println(levelMenu);
+        return levelMenu;
+    }
+
+    /**
+     * 递归查询所有菜单下的子菜单
+     *
+     * @param root 菜单
+     * @param all  子菜单
+     * @return 所有菜单
+     */
+    private List<PmsCategoryEntity> getChildren(PmsCategoryEntity root, List<PmsCategoryEntity> all) {
+        List<PmsCategoryEntity> pmsCategoryEntities = all.stream()
+                .filter(pmsCategoryEntity -> {
+                    return pmsCategoryEntity.getParentCid() == root.getCatId();
+                })
+                .map(pmsCategoryEntity -> {
+                    pmsCategoryEntity.setChildren(getChildren(pmsCategoryEntity, all));
+                    return pmsCategoryEntity;
+                })
+                .sorted(Comparator.comparingInt(sort -> (sort.getSort() == null ? 0 : sort.getSort())))
+                .collect(Collectors.toList());
+        return pmsCategoryEntities;
+    }
+
 
 }
