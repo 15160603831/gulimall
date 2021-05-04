c5747950471946cf86eee5f77dc31362fdea9138
hwj <123@qq.com>
2021-05-04 16:15:53 +0800

    feat(*)动态获取配置文件

--- mall-coupon/src/main/java/com/hwj/mall/coupon/controller/SmsCouponController.java
+++ mall-coupon/src/main/java/com/hwj/mall/coupon/controller/SmsCouponController.java
@@ -5,13 +5,13 @@ import java.util.Map;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import com.hwj.mall.coupon.entity.SmsCouponEntity;
 import com.hwj.mall.coupon.service.SmsCouponService;
 import com.hwj.common.utils.PageUtils;
 import com.hwj.common.utils.R;
-
 
 
 /**
@@ -27,18 +27,31 @@ public class SmsCouponController {
     @Autowired
     private SmsCouponService smsCouponService;
 
+    @Value("${coupon.user.name}")
+    private String name;
+    @Value("${coupon.user.age}")
+    private String age;
+
+    /**
+     * 获取配置文件信息
+     *
+     * @return
+     */
+    @GetMapping("test")
+    public R test() { return R.ok().put("name", name).put("age", age); }
+
     @RequestMapping("/member/list")
-    public R memberCoupon(){
+    public R memberCoupon() {
         SmsCouponEntity entity = new SmsCouponEntity();
         entity.setCouponName("满100减100");
-        return R.ok().put("coupons",Arrays.asList(entity));
+        return R.ok().put("coupons", Arrays.asList(entity));
     }
 
     /**
      * 列表
      */
     @RequestMapping("/list")
-    public R list(@RequestParam Map<String, Object> params){
+    public R list(@RequestParam Map<String, Object> params) {
         PageUtils page = smsCouponService.queryPage(params);
         return R.ok().put("page", page);
     }
@@ -48,8 +61,8 @@ public class SmsCouponController {
      * 信息
      */
     @RequestMapping("/info/{id}")
-    public R info(@PathVariable("id") Long id){
-		SmsCouponEntity smsCoupon = smsCouponService.getById(id);
+    public R info(@PathVariable("id") Long id) {
+        SmsCouponEntity smsCoupon = smsCouponService.getById(id);
 
         return R.ok().put("smsCoupon", smsCoupon);
     }
@@ -58,8 +71,8 @@ public class SmsCouponController {
      * 保存
      */
     @RequestMapping("/save")
-    public R save(@RequestBody SmsCouponEntity smsCoupon){
-		smsCouponService.save(smsCoupon);
+    public R save(@RequestBody SmsCouponEntity smsCoupon) {
+        smsCouponService.save(smsCoupon);
 
         return R.ok();
     }
@@ -68,8 +81,8 @@ public class SmsCouponController {
      * 修改
      */
     @RequestMapping("/update")
-    public R update(@RequestBody SmsCouponEntity smsCoupon){
-		smsCouponService.updateById(smsCoupon);
+    public R update(@RequestBody SmsCouponEntity smsCoupon) {
+        smsCouponService.updateById(smsCoupon);
 
         return R.ok();
     }
@@ -78,8 +91,8 @@ public class SmsCouponController {
      * 删除
      */
     @RequestMapping("/delete")
-    public R delete(@RequestBody Long[] ids){
-		smsCouponService.removeByIds(Arrays.asList(ids));
+    public R delete(@RequestBody Long[] ids) {
+        smsCouponService.removeByIds(Arrays.asList(ids));
 
         return R.ok();
     }
