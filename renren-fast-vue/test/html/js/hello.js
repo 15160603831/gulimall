e366a23c1d1995f41a3b5c57770d794de6e0aa84
hwj <123@qq.com>
2021-05-05 18:18:33 +0800

    feat(*)renren-fase-vue

---
+++ renren-fast-vue/test/html/js/hello.js
@@ -0,0 +1,7 @@
+export const util = {
+    sum(a, b) {
+        return a + b;
+    }
+}
+
+export {util}
---
+++ renren-fast-vue/test/html/js/user.js
@@ -0,0 +1,7 @@
+var name = "jack"
+var age = 21
+function add(a,b){
+    return a + b;
+}
+// 导出变量和函数
+export {name,age,add}
