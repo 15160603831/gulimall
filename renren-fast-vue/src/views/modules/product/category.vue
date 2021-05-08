5c832c7e349bf6049b3321d8fff5808abbdd7adc
hwj <1571317440@qq.com>
2021-05-08 17:32:36 +0800

    feat(*)新增商品列表拖拽功能

--- renren-fast-vue/src/views/modules/product/category.vue
+++ renren-fast-vue/src/views/modules/product/category.vue
@@ -6,43 +6,56 @@
              node-key="catId"
              :props="defaultProps"
              :expand-on-click-node="false"
-             :default-expanded-keys="expandedkey">
+             :default-expanded-keys="expandedkey"
+             draggable
+             :allow-drop="allowDrop">
       <span class="custom-tree-node"
             slot-scope="{ node, data }">
         <span>{{ node.label }}</span>
         <span>
-          <el-button
-            v-if="node.level <= 2"
-            type="text"
-            size="mini"
-            @click="() => append(data)"
-          >
+          <el-button v-if="node.level <= 2"
+                     type="text"
+                     size="mini"
+                     @click="() => append(data)">
             新增
           </el-button>
-          <el-button type="text" size="mini" @click="() => edit(data)">
+          <el-button type="text"
+                     size="mini"
+                     @click="() => edit(data)">
             修改
           </el-button>
-          <el-button
-            v-if="node.childNodes.length == 0"
-            type="text"
-            size="mini"
-            @click="() => remove(node, data)"
-          >
+          <el-button v-if="node.childNodes.length == 0"
+                     type="text"
+                     size="mini"
+                     @click="() => remove(node, data)">
             删除
           </el-button>
         </span>
       </span>
     </el-tree>
 
-    <el-dialog :dialogName="提示" :visible.sync="dialogVisible">
+    <el-dialog :title="title"
+               :visible.sync="dialogVisible"
+               :close-on-click-modal="false">
       <el-form :model="category">
         <el-form-item label="商品分类">
-          <el-input v-model="category.name" autocomplete="off"></el-input>
+          <el-input v-model="category.name"
+                    autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="图标">
+          <el-input v-model="category.icon"
+                    autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="计量单位">
+          <el-input v-model="category.productUnit"
+                    autocomplete="off"></el-input>
         </el-form-item>
       </el-form>
-      <span slot="footer" class="dialog-footer">
+      <span slot="footer"
+            class="dialog-footer">
         <el-button @click="dialogVisible = false">取 消</el-button>
-        <el-button type="primary" @click="submitData">确 定</el-button>
+        <el-button type="primary"
+                   @click="submitData">确 定</el-button>
       </span>
     </el-dialog>
   </div>
@@ -56,10 +69,11 @@ export default {
   //import引入的组件需要注入到对象中才能使用
   components: { name: "" },
   props: {},
-  data() {
+  data () {
     //这里存放数据
     return {
-      dialogName:"提示",
+      maxLevel: 0,
+      title: "",
       dialogType: "",
       menus: [],
       category: {
@@ -69,6 +83,9 @@ export default {
         showStaus: 1,
         sort: 0,
         catId: null,
+        icon: null,
+        productUnit: null,
+        productCount: null
       },
       dialogVisible: false,
       expandedkey: [],
@@ -85,7 +102,7 @@ export default {
   //方法集合
   methods: {
     //获取商品菜单
-    getMenus() {
+    getMenus () {
       this.$http({
         url: this.$http.adornUrl("/product/pmscategory/list/tree"),
         method: "get",
@@ -93,18 +110,39 @@ export default {
         this.menus = data.data;
       });
     },
+    //拖拽时判定目标节点能否被放置
+    allowDrop (draggingNode, dropNode, type) {
+      // //b被拖动的节点总层数
+      // console.log("draggingNode", draggingNode, dropNode, type)
+      // var level =  (draggingNode);
+      this.countNodeLevel(draggingNode.data);
+      //当前正在拖动的节点不大于3
+      console.log("深度", this.maxLevel);
+      return false;
+    },
+    countNodeLevel (node) {
+      //求出所有子节点最大深度
+      if (node.children != null && node.children.length > 0) {
+        for (let i = 0; i < node.children.length; i++) {
+          if (node.children[i].catLevel > this.maxLevel) {
+            this.maxLevel = node.catLevel[i].catLevel;
+          }
+          this.countNodeLevel(node.catLevel[i]);
+        }
+      }
+    },
 
-    submitData() {
-      if(this.dialogType=="append"){
+    submitData () {
+      if (this.dialogType == "append") {
         this.addCategory();
       }
-      if(this.dialogType=="edit"){
+      if (this.dialogType == "edit") {
         this.editCategory();
       }
     },
 
     //删除
-    remove(node, data) {
+    remove (node, data) {
       console.log("remove", node, data);
       this.$confirm(`此操作将永久删除【${data.name}】菜单, 是否继续?`, "提示", {
         confirmButtonText: "确定",
@@ -135,16 +173,25 @@ export default {
           });
         });
     },
-    append(data) {
+    //新增model
+    append (data) {
       console.log("append", data);
       this.dialogType = "append";
       this.dialogVisible = true;
-      this.dialogName="添加商品分类"
+      this.title = "添加商品分类"
       this.category.parentCid = data.catId;
       this.category.catLevel = data.catLevel * 1 + 1;
+
+      this.category.name = null;
+      this.category.catId = null;
+      this.category.icon = "";
+      this.category.productUnit = "";
+      this.category.sort = 0;
+      this.category.showStaus = 1;
+
     },
     //添加三级分类
-    addCategory() {
+    addCategory () {
       console.log("添加三级分类" + this.category);
       this.$http({
         url: this.$http.adornUrl("/product/pmscategory/save"),
@@ -163,36 +210,59 @@ export default {
         this.expandedkey = [this.category.parentCid];
       });
     },
-    //修改菜单名称
-    edit(data) {
+    //修改菜单名称 model
+    edit (data) {
       console.log("修改啦" + data);
       this.dialogType = "edit";
-      this.dialogName="修改商品分类"
+      this.title = "修改商品分类"
       this.dialogVisible = true;
-      this.category.name = data.name;
-      this.category.catId = data.catId;
-    },
-    editCategory(){
+      //发送请求查询当前节点最新的数据
       this.$http({
-      url: this.$http.adornUrl(''),
-      method: 'post',
-      data: this.$http.adornData(data, false)
-      }).then(({ data }) => { });
+        url: this.$http.adornUrl(`/product/pmscategory/info/${data.catId}`),
+        method: 'get',
+        params: this.$http.adornParams({})
+      }).then(({ data }) => {
+        this.category.name = data.data.name;
+        this.category.catId = data.data.catId;
+        this.category.icon = data.data.icon;
+        this.category.productUnit = data.data.productUnit;
+        this.category.parentCid = data.data.parentCid;
+      })
+    },
+    //修改分类名称
+    editCategory () {
+      var { catId, name, icon, productUnit } = this.category;
+      this.$http({
+        url: this.$http.adornUrl('/product/pmscategory/update'),
+        method: 'post',
+        data: this.$http.adornData({ catId, name, icon, productUnit }, false)
+      }).then(({ data }) => {
+        this.$message({
+          type: "success",
+          message: "菜单修改成功!",
+        });
+        //关闭对话框
+        this.dialogVisible = false;
+        //刷新新菜单
+        this.getMenus();
+        //设置需要打开的菜单
+        this.expandedkey = [this.category.parentCid];
+      });
     }
   },
   //生命周期 - 创建完成（可以访问当前this实例）
-  created() {
+  created () {
     this.getMenus();
   },
   //生命周期 - 挂载完成（可以访问DOM元素）
-  mounted() {},
-  beforeCreate() {}, //生命周期 - 创建之前
-  beforeMount() {}, //生命周期 - 挂载之前
-  beforeUpdate() {}, //生命周期 - 更新之前
-  updated() {}, //生命周期 - 更新之后
-  beforeDestroy() {}, //生命周期 - 销毁之前
-  destroyed() {}, //生命周期 - 销毁完成
-  activated() {}, //如果页面有keep-alive缓存功能，这个函数会触发
+  mounted () { },
+  beforeCreate () { }, //生命周期 - 创建之前
+  beforeMount () { }, //生命周期 - 挂载之前
+  beforeUpdate () { }, //生命周期 - 更新之前
+  updated () { }, //生命周期 - 更新之后
+  beforeDestroy () { }, //生命周期 - 销毁之前
+  destroyed () { }, //生命周期 - 销毁完成
+  activated () { }, //如果页面有keep-alive缓存功能，这个函数会触发
 };
 </script>
 <style scoped>
