4940cb2a976c1f991c34675e212ec6f7210754f8
hwj <123@qq.com>
2021-05-10 00:00:18 +0800

    feat(*)测试类中增加@RunWith(SpringRunner.class)，需要该才能实例化到spring容器中，自动注入才能生效

--- mall-third-party/src/test/java/com/hwj/mall/thirdparty/MallThirdPartyApplicationTests.java
+++ mall-third-party/src/test/java/com/hwj/mall/thirdparty/MallThirdPartyApplicationTests.java
@@ -3,14 +3,17 @@ package com.hwj.mall.thirdparty;
 
 import com.aliyun.oss.OSSClient;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 
 @SpringBootTest
+@RunWith(SpringRunner.class)
 public class MallThirdPartyApplicationTests {
 
     @Autowired
