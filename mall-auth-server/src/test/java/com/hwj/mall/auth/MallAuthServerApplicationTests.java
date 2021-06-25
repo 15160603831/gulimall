9e0cdeffa302083415eb4a9d424f0e1eb735b053
hwj <1571317440@qq.com>
2021-06-25 14:30:01 +0800

    feat(*)认证系统环境搭建

---
+++ mall-auth-server/src/test/java/com/hwj/mall/auth/MallAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hwj.mall.auth;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallAuthServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
