dc472f343dcff7f2368dec722caf148b9745b667
hwj <1571317440@qq.com>
2021-07-02 18:29:27 +0800

    feat(*)订单（二）引入session和thread线程池

--- mall-order/src/main/java/com/hwj/mall/order/MallOrderApplication.java
+++ mall-order/src/main/java/com/hwj/mall/order/MallOrderApplication.java
@@ -4,7 +4,9 @@ import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
+@EnableRedisHttpSession //整合session
 @EnableRabbit   //rabbit消息队列
 @EnableDiscoveryClient
 @SpringBootApplication
